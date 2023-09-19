47a72170ac4910805424eeee7665f41ba47a380a
Micha Zawadzki <michalzawadzki@MacBook-Air-Micha.local>
2023-09-19 22:41:40 +0200

    Add alt

--- info_side/src/components/Navbar.js
+++ info_side/src/components/Navbar.js
@@ -4,7 +4,7 @@ import logo from "../img/react-icon-small.png"
 export default function Navbar(){
     return (
         <nav>
-            <img src={logo} className="nav--icon" />
+            <img src={logo} alt="obrazek" className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Fun facts</h4>
         </nav>
